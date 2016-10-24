268e82a780b6e88bb151b1061171a00cd1afe6e1
zouzhisheng <zzsshun13@gmail.com>
2016-10-24 22:30:14 +0800

    add new input to edit the path

--- 000/000.py
+++ 000/000.py
@@ -9,13 +9,13 @@ def add_num(img):
     font = ImageFont.truetype(r'C:/windows/fonts/Arial.ttf', size=50)
     fillcolor = "#ff0000"
     width, height = img.size
-    
-    draw.text((width-50, 0),'99', font=font,fill=fillcolor)
-    img.save(r'D:\reset.jpg','jpeg')
+    To_addr = input("请输入存储路径：\n")
+    draw.text((width-100, 0),'99+', font=font,fill=fillcolor)
+    img.save(To_addr,'jpeg')
     return 0
 if __name__ == '__main__'  :
-    
-    img = Image.open(r'E:\desktop_pic\1.jpg')
+    addr = input("请输入需要修改的图片路径：\n")
+    img = Image.open(addr)
     add_num(img)
     
     
